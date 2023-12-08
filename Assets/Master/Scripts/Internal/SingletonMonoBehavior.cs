27a4ff134695ea163cd7ec23a2f4d237f7236c5e
charlieBoyer <charlieboyer@outlook.fr>
2023-12-08 17:39:48 +0100

    Start SceneLoader and scenes transitions
    + Add empty Menu and Introduction scenes
    + Import SingletonMonoBehavior Script in new Internal folder
    + Start SceneLoader Component
    * Update build settings for future tests

---
+++ Assets/Master/Scripts/Internal/SingletonMonoBehavior.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Charlie.Scripts.Internal
+{
+    public abstract class SingletonMonoBehavior<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+
+                    if (_instance == null)
+                    {
+                        GameObject singletonObject = new GameObject(typeof(T).Name);
+                        _instance = singletonObject.AddComponent<T>();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
