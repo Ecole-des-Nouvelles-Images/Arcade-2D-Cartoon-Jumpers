4f06a9feba1e440c0e26f022caa02dc665e5b756
charlieBoyer <charlieboyer@outlook.fr>
2023-12-08 15:40:55 +0100

    Merge and Integrated animations test, new Player component and Enemies' commandPattern
    [Scene]
    * Update scene with all following changes
    * Update changed components
    - Remove ennemies for precise integration later
    * Rename scene from "Prototype" to "Game"
    
    [Animations]
    + Import Marine's first animations and Animation controllers
    * Made small adjustments to Idle/Dash transitions to counteract speed stuttering from bounciness
    
    [Data]
    + Import ScriptableObjects (from Player actions to Commands)
    
    [Sprites]
    + Import various Enemy's sprites
    + Import Player's sprites, including Dash
    * Tidy-up file hierarchy
    + Add "Environment" folder with first test of level
    + Add missing cursor prefab to UI folder
    
    [Prefabs]
    * Update Player, Camera and Environment prefabs
    * Update components of EyeEnemy prefab
    
    [Dev branches]
    - Integrate working changes and flush folders
    + Copy clean "Game" scene for further test and development
    
    [Misc]
    - Remove "RuleTile" menu asset from Samples
    * Update tags and Layers : add 2 sorting layers "Foreground" and "Background"
    
    [Scripts]
    + Import Alex's Enemy scripts
    - Remove useless EyeEnemyController ?
    * Move SO classes to SO folder
    + Add Player satellite objects
    + Add new Player component
    * Update PlayerController
    * Remade CameraController
    + Import Utils folder from Alex

---
+++ Assets/Master/Scripts/SO/DashSO.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Master.Scripts.Player;
+
+namespace Master.Scripts.SO
+{
+    [CreateAssetMenu(menuName = "Player Action/New Dash Type", fileName = "Dash", order = 3)]
+    public class DashSO: ScriptableObject
+    {
+        [Header("Base Stats")]
+        public int Power;
+        public float Velocity = 80f;
+        public float Cooldown = 1f;
+
+        public Dash ConstructDashObject()
+        {
+            return new Dash(Power, Velocity, Cooldown);
+        }
+    }
+}
